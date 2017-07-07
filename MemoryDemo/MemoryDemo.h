02d889a963376de362e886efc3b28ca1ac3d9cd6
lianlian <kuliantnt@gmail.com>
2017-07-07 08:48:19 +0800

    2017年07月07日08:48:05

--- MemoryDemo/MemoryDemo.h
+++ MemoryDemo/MemoryDemo.h
@@ -13,6 +13,11 @@ class MemoryDemo
     virtual ~MemoryDemo(){}
     void* operator new(std::size_t size);
     void operator delete (void* ptr) noexcept ;
+    void* operator new[] (std::size_t size);
+    void* operator delete[] (void* ptr) noexcept;
+    void* operator new (std::size_t size, const std::nothrow_t&) noexcept;
+    void* operator delete (void* ptr, const std::nothrow_t&) noexcept;
+
 
 
 };
