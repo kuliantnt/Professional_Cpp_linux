3bd74ac092c982b15603dfcbcfd1318d1fdab982
lianlian <kuliantnt@gmail.com>
2017-07-19 09:33:03 +0800

    2017年07月19日09:32:56

--- STL_Container/main.cpp
+++ STL_Container/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <list>
+#include <string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::list<std::string> dictionary {"aardvark","ambulance"};
+    std::list<std::string> b_words {"bathos","balderdash"};
+    dictionary.push_back("canticle");
+    dictionary.push_back("consumerism");
+    if(b_words.size() > 0){
+        auto iterLastB = --(std::cend(b_words));
+        auto it = std::cbegin(dictionary);
+        for(;it!= std::cend(dictionary);++it){
+            if(*it > *iterLastB){
+                break;
+            }
+        }
+        dictionary.splice(it,b_words);
+    }
+    for(const auto & word : dictionary){
+        std::cout << word << std::endl;
+    }
     return 0;
 }
