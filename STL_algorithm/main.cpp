1fe9671a274ff9bb0ce69220fe52f45de4978eee
kuliantnt <kuliantnt@gmail.com>
2017-08-07 17:10:08 +0800

    2017-8-7

--- STL_algorithm/main.cpp
+++ STL_algorithm/main.cpp
@@ -3,10 +3,14 @@
 #include <vector>
 #include <numeric>
 #include <cmath>
+#include <memory>
+#include <functional>
 
 bool perfectScore(int num);
+std::function <int(void)> multiplyBy2Lambda(int x);
+void testCallback(const std::vector<int>& vec,const std::function<bool(int)>& callback);
 
-int main() {
+int main(){
     /**
      * algorithm find()
      */
@@ -77,7 +81,7 @@ int main() {
     /**
      * Using auto lambda
      */
-    std::vector<int> ints{10, 55, 101, 200};
+    /*std::vector<int> ints{10, 55, 101, 200};
     std::vector<double> doubles{11.1, 55.5, 200.2};
 
     auto isGreaterThan100 = [](auto i) { return i > 100; };
@@ -90,7 +94,31 @@ int main() {
     auto it2 = std::find_if(std::cbegin(doubles), std::cend(doubles), isGreaterThan100);
     if (it2 != std::cend(doubles)) {
         std::cout << "Found a value > 100: " << *it2 << std::endl;
-    }
+    }*/
+    /**
+     * Using lambda initialized list
+     */
+    /*const double pi = 3.1415;
+    auto myPtr = std::make_unique<double>(3.1415);
+    auto myLambda = [p = std::move(myPtr)]{
+        std::cout << *p;
+    };
+//    auto myLambda = [myCapture = "pi: ", pi]{std::cout << myCapture << pi;};
+
+    //必须要执行一遍才行嘛。。。。
+    myLambda();*/
+    /**
+     * Using functional;
+     */
+/*
+    std::function<int(void)>fn = multiplyBy2Lambda(2);
+    std::cout << fn();*/
+    /**
+     * Using Parameter lambda
+     */
+    std::vector<int> vec1{1,2,3,4,5,6,7,8,9,10};
+    testCallback(vec1,[](int i){return i < 6;});
+
 }
 bool perfectScore(int num){
     return (num>=100);
@@ -111,4 +139,17 @@ double geometricMeanLambda(const std::vector<int> & nums) {
     double mult = std::accumulate(std::cbegin(nums), std::cend(nums), 1,
                                   [](int num1, int num2) { return num1, num2; });
     return pow(mult, 1.0 / nums.size());
+}
+std::function <int(void)> multiplyBy2Lambda(int x)
+{
+    return [x]{return 2*x;};
+}
+void testCallback(const std::vector<int>& vec,
+        const std::function<bool(int)>& callback) {
+    for (const auto &i : vec) {
+        if (!callback(i))
+            break;
+        std::cout << i << "\t";
+    }
+    std::cout << std::endl;
 }
