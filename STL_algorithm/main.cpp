8a7582a2cd3f4675453de453a81aef1a0ee17ed9
kuliantnt <kuliantnt@gmail.com>
2017-08-07 14:21:38 +0800

    2017-8-7

--- STL_algorithm/main.cpp
+++ STL_algorithm/main.cpp
@@ -58,22 +58,39 @@ int main() {
     /**
      * algorithm find_if lambda example
      */
-   /* int num;
-    std::vector<int> myVector;
-    while(true){
-        std::cout << "Enter a test score to add (0 to stop):";
-        std::cin>> num;
-        if(num == 0)
-            break;
-        myVector.push_back(num);
+    /* int num;
+     std::vector<int> myVector;
+     while(true){
+         std::cout << "Enter a test score to add (0 to stop):";
+         std::cin>> num;
+         if(num == 0)
+             break;
+         myVector.push_back(num);
+     }
+     auto endIt = std::cend(myVector);
+     auto it = std::find_if(std::cbegin(myVector),endIt,[](int i){return i>100;});
+     if(it == endIt)
+         std::cout << "No perfect scores "<< std::endl;
+     else
+         std::cout << "Fount a \"perfect\" score of "<< *it << std::endl;
+     return 0;*/
+    /**
+     * Using auto lambda
+     */
+    std::vector<int> ints{10, 55, 101, 200};
+    std::vector<double> doubles{11.1, 55.5, 200.2};
+
+    auto isGreaterThan100 = [](auto i) { return i > 100; };
+
+    auto it1 = std::find_if(std::cbegin(ints), std::cend(ints), isGreaterThan100);
+    if (it1 != std::cend(ints)) {
+        std::cout << "Found a value > 100: " << *it1 << std::endl;
     }
-    auto endIt = std::cend(myVector);
-    auto it = std::find_if(std::cbegin(myVector),endIt,[](int i){return i>100;});
-    if(it == endIt)
-        std::cout << "No perfect scores "<< std::endl;
-    else
-        std::cout << "Fount a \"perfect\" score of "<< *it << std::endl;
-    return 0;*/
+
+    auto it2 = std::find_if(std::cbegin(doubles), std::cend(doubles), isGreaterThan100);
+    if (it2 != std::cend(doubles)) {
+        std::cout << "Found a value > 100: " << *it2 << std::endl;
+    }
 }
 bool perfectScore(int num){
     return (num>=100);
